31a0e400c686a52e9527a94ac9472e50ee3e2711
Sobihan Mahendran <sobihan.mahedran@epitech.eu>
2021-10-19 14:50:58 +0200

    [ADDED] WebView

---
+++ redditech/lib/Pages/LoginPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+
+class LoginPage extends StatelessWidget {
+  void signinReddit() async {
+    // const String id = 'd24KuCnv30paH6vY2-61Bw';
+  }
+  void signInReddit() async {
+    // final String id = "d24KuCnv30paH6vY2-61Bw";
+    final response = await FlutterWebAuth.authenticate(
+        url:
+            "https://www.reddit.com/api/v1/authorize?client_id=d24KuCnv30paH6vY2-61Bw&response_type=code&state=TEST&redirect_uri=com.example.redditech://callback&scope=read",
+        callbackUrlScheme: "com.example.redditech://callback");
+    print(response.length);
+  }
+
+  Widget build(BuildContext context) {
+    print("Hello");
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 20), primary: Colors.orange[400]);
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: signInReddit,
+            child: const Text('Connect with Reddit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
