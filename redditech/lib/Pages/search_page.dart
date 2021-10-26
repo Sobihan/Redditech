e30c468ccdcf5c2287044f835f5ea613ac78efe0
Sobihan Mahendran <sobihan.mahedran@epitech.eu>
2021-10-26 09:31:44 +0200

    [ADD] Search bar

--- redditech/lib/Pages/search_page.dart
+++ redditech/lib/Pages/search_page.dart
@@ -8,8 +8,38 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPage extends State<SearchPage> {
+  final TextEditingController _controller = TextEditingController();
+
+  void onPressed() {
+    print(_controller.text.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Search page ${widget.accessToken}');
+    return TextField(
+      textInputAction: TextInputAction.go,
+      onSubmitted: (value) => onPressed(),
+      controller: _controller,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Enter a search term',
+          suffixIcon: Container(
+            width: 100,
+            child: Row(
+              children: [
+                IconButton(
+                  color: Colors.grey,
+                  icon: Icon(Icons.clear),
+                  onPressed: _controller.clear,
+                ),
+                IconButton(
+                  color: Colors.red,
+                  icon: Icon(Icons.send),
+                  onPressed: onPressed,
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
